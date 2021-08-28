0492dbd4ea73e5cf8ef5286148e609be4e478346
edmondfrancoismillionnaires <edmondfrancoismillionnaires@gmail.com>
2021-08-28 21:24:12 +0000

    add test

---
+++ src/components/SearchBar.test.js
@@ -0,0 +1,41 @@
+const SearchBar = require("./SearchBar")
+// @ponicode
+describe("onInputChange", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new SearchBar.default()
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.onInputChange({ target: { value: "Dillenberg" } })
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction = () => {
+            inst.onInputChange({ target: { value: "elio@example.com" } })
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let callFunction = () => {
+            inst.onInputChange({ target: { value: "Elio" } })
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("3", () => {
+        let callFunction = () => {
+            inst.onInputChange(undefined)
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
